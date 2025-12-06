818f477639f1d1aade7207b3d536201c5b64c86a
Manashi Ramdam <rdm3894@gmail.com>
2025-12-06 11:49:55 +0545

    Add initial JavaScript file

---
+++ app.js
@@ -0,0 +1,94 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset_btn");
+let newGameBtn = document.querySelector("#newBtn");
+let msgContainer = document.querySelector(".msg_Container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+
+let count = 0;
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+const resetGame = () => {
+    turnO = true;
+    count = 0;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click", ()=>{
+        console.log("Box was Clicked.");
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled = true;
+        count++;
+
+        checkWinner();
+    });
+});
+
+
+
+const disableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = true ;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = false ;
+        box.innerText="";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congrats!!! Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const showDraw = () => {
+    msg.innerText = `The match was a DRAW!!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const checkWinner=()=>{
+    for(let pattern of winPatterns){   
+        let pos1= boxes[pattern[0]].innerText; 
+        let pos2= boxes[pattern[1]].innerText;
+        let pos3= boxes[pattern[2]].innerText;
+
+        if (pos1 != "" && pos2 != "" && pos3 != ""){
+            if (pos1 == pos2 && pos2 ==pos3 ){
+                showWinner(pos1);
+                return;
+            }
+
+        }
+    }
+
+    if (count ==9){
+        showDraw();
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
